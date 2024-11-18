2126898d37227fd7268486eeb86b6428bd301a47
JurlyX <161505209+JurlyX@users.noreply.github.com>
2024-11-18 09:37:04 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+var a = document.getElementById('Secret'); 
+a.addEventListener('mousedown', function Redirect() {
+                     window.location.href = "secretpage.htm";
+                   }, false);
+
+
+function redirectToBanana() {
+  window.location.href = "page3.htm";
+}
+
+function redirectToMonkey() {
+  window.location.href = "page2.htm";
+}
+
+function RedirectToHome() {
+  window.location.href = "page1.htm";
+}
+
+function RedirectToPage4() {
+  window.location.href = "page4.htm";
+}
+
+function RedirectTo4everClicks() {
+  window.location.href = "Foreverclicks.htm";
+}
+
+function Redirect() {
+  window.location.href = "secretpage.htm";
+}
+
+function RedirectToGrind() {
+  window.location.href = "Grindcraft.htm";
+}
+
+function RedirectToDoge() {
+  window.location.href = "Doge.htm";
+}
+
+function RedirectToDoodle() {
+  window.location.href = "Doodle.htm";
+}
+
+function RedirectToMelon() {
+  window.location.href = "Melon.htm";
+}
+
+function RedirectToFeud() {
+  window.location.href = "Feud.htm";
+}
+
+function RedirectToWordle() {
+  window.location.href = "Wordle.htm";
+}
+
+function Redirect2() {
+  window.location.href = "secreterpage.htm";
+}
+
+
+
+function RedirectToBit() {
+  window.location.href = "Bit.htm";
+}
+
+
+
---
+++ script2.js
@@ -0,0 +1,24 @@
+var b = document.getElementById('Secreter'); 
+b.addEventListener('mousedown', function Redirect2() {
+                     window.location.href = "secreterpage.htm";
+                   }, false);
+
+function RedirectToRobux() {
+  window.location.href = "robuxclicker.htm";
+}
+
+function RedirectToMine() {
+  window.location.href = "Mine.htm";
+}
+
+function RedirectToMine2() {
+  window.location.href = "Minecraft.htm";
+}
+
+function RedirectToNo() {
+  window.location.href = "No.htm";
+}
+
+function RedirectToMario() {
+  window.location.href = "Mario.htm";
+}
